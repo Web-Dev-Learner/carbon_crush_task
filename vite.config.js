6e1b1bdfc72bade783e03053c075401b02ac1456
Web-Dev-Learner <shribabe2@gmail.com>
2025-03-22 13:55:05 +0530

    Configured GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/carbon_crunch_task/",
   plugins: [react() ,tailwindcss()],
   
 })
